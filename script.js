f3b86cad9fd3815975c90d8ab78327f55ffe1448
VyncentCoquia <vyncentlanxyrcoquia@gmail.com>
2026-07-06 12:00:48 +0800

    Created function to add user input on array

--- script.js
+++ script.js
@@ -6,22 +6,32 @@ const expenseBtn =
 expenseBtn.addEventListener("click", addExpense);
 
 function addExpense(){
+    
     const expenseName =
         document.getElementById("expenseName-input").value;
     
-    const amount =
+    const expenseAmount =
         document.getElementById("amount-input").value;
         
-    const category =
+    const expenseCategory =
         document.getElementById("category-input").value;    
 
-    const date =
+    const expenseDate =
         document.getElementById("date-input").value;    
 
-
-    console.log(expenseName);
-    console.log(amount);   
-    console.log(category);
-    console.log(date); 
+    let expense =
+        {
+            name: expenseName,
+            amount: expenseAmount,
+            category: expenseCategory,
+            date: expenseDate
+        }
+    ;
+        
+        expenses.push(expense);
+        console.log(expenses);
+   
 };
 
+//1.  it retrieves the string or integer and then passes it to the variable for storage for data manipulaton
+//2. parsefloat() is for decimals because it was an amount and all know that when we store money it is not just a whole number, so we need to consider the value to converted to float
